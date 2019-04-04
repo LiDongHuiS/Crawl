e770222d947c9cddc0887dd4b19961448efa7995
LiDongHuiS <ldh1994w@163.com>
2019-04-04 11:17:31 +0800

    图片抓取-更新中

---
+++ Img.py
@@ -0,0 +1,13 @@
+# encoding : utf-8
+
+import requests
+from lxml import etree
+from bs4 import BeautifulSoup
+
+
+class ImgCrawl:
+    def __init__(self):
+        self.url = 'https://www.58pic.com/'
+        
+
+
